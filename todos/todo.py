cfdec5b1cfcb32fd0ed0ecce7e5deca97988ee6a
DeMonA1 <demonagrail1@gmail.com>
2024-08-27 17:21:45 +0300

    implementation of templates; requests args

--- todos/todo.py
+++ todos/todo.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Request, Depends
+from fastapi.templating import Jinja2Templates
 from model import Todo, TodoItem, TodoItems
 
 
@@ -6,24 +7,34 @@ todo_router = APIRouter()
 
 todo_list = []
 
+templates = Jinja2Templates(directory="templates/")
+
 
 @todo_router.post("/todo")
-async def add_todo(todo: Todo, status_code=201) -> dict:
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
     todo_list.append(todo)
-    return {"message": "Todo added successfully"}
+    return templates.TemplateResponse("todo.html",
+                                      {"request": request,
+                                       "todos": todo_list})
 
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todo() -> dict:
-    return {"todos": todo_list}
+async def retrieve_todo(request: Request):
+    return templates.TemplateResponse("todo.html",
+                                      {"request": request,
+                                       "todos": todo_list})
 
 
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID \
-    of the todo to retrieve.")) -> dict:
+async def get_single_todo(request: Request, todo_id: int = 
+            Path(..., title="The ID of the todo to retrieve.")):
     for todo in todo_list:
         if todo.id == todo_id:
-            return {"todo": todo}
+            return templates.TemplateResponse("todo.html",
+                                      {"request": request,
+                                       "todo": todo})
+            
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Todo with supplied ID doesn't exist",
@@ -31,12 +42,13 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID \
 
 
 @todo_router.put("/todo/{todo_id}")
-async def update_todo(todo_data: TodoItem, todo_id: int =
-                      Path(..., title="The ID of the todo to be updated")) -> dict:
+async def update_todo(request: Request, todo_data: TodoItem, 
+            todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             todo.item = todo_data.item
             return {"message": "Todo updated successfully."}
+        
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Todo with supplied ID doesn't exist",
@@ -44,12 +56,13 @@ async def update_todo(todo_data: TodoItem, todo_id: int =
 
 
 @todo_router.delete("/todo/{todo_id}")
-async def delete_single_todo(todo_id: int) -> dict:
+async def delete_single_todo(request: Request, todo_id: int) -> dict:
     for index in range(len(todo_list)):
         todo = todo_list[index]
         if todo.id == todo_id:
             todo_list.pop(index)
             return {"message": "Todo deleted successfully."}
+        
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Todo with supplied ID doesn't exist",
