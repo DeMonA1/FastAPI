d2e655cbb7a6acd6b9389a2cf8717ae89d707048
DeMonA1 <demonagrail1@gmail.com>
2024-08-27 17:18:42 +0300

    Depends for the add_todo(as_form)

--- todos/model.py
+++ todos/model.py
@@ -1,10 +1,17 @@
 from pydantic import BaseModel
-from typing import List
+from fastapi import Form
+from typing import List, Optional
+
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int] = None
     item: str
     
+    @classmethod
+    def as_form(cls, item: str = Form(...)): 
+        return cls(item=item)
+
+
     class Config:
         json_schema_extra = {
             "example": {
