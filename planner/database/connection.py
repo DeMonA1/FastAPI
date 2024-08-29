a9551bba3f68a3729a7df0b821e02493b35aa517
DeMonA1 <demonagrail1@gmail.com>
2024-08-29 14:23:30 +0300

    verify and create auth token

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -11,6 +11,7 @@ from models.users import User
 
 class Settings(BaseSettings):
     DATABASE_URL: Optional[str] = None
+    SECRET_KEY: Optional[str] = None
     
     async def initialize_database(self):
         client = AsyncIOMotorClient(self.DATABASE_URL)
