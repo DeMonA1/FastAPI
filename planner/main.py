9e051c446f45d1f0a17a007faba7fff62d5e699a
DeMonA1 <demonagrail1@gmail.com>
2024-08-29 09:55:44 +0300

    implementation of MongoDb

--- planner/main.py
+++ planner/main.py
@@ -1,18 +1,33 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import Settings
 
-import uvicorn
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await settings.initialize_database()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+
+settings = Settings()
 
 # Register routes
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
